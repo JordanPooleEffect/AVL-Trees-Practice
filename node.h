2e61b32c846036122f5aae3f0f577a1c164b2a2c
Kenshi123 <zmtabinas@gmail.com>
2024-04-22 21:49:45 +0800

    NO ANSWERS

---
+++ node.h
@@ -0,0 +1,13 @@
+#include <algorithm>
+using namespace std;
+struct node {
+    node* parent;
+    node* right;
+    node* left;
+    int elem;
+
+    // TODO paste your height method here
+    int height() {
+        
+    }
+};
